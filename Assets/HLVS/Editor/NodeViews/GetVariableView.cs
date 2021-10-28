3bda398e204934f525f11b407e4ede5c88b09fd0
Greebling <Desktop+Greebling@users.noreply.github.com>
2021-10-28 20:40:09 +0200

    Cleanup GetVariable input

--- Assets/HLVS/Editor/NodeViews/GetVariableView.cs
+++ Assets/HLVS/Editor/NodeViews/GetVariableView.cs
@@ -1,9 +1,8 @@
-using System;
 using GraphProcessor;
 using HLVS.Nodes;
+using UnityEditor;
 using UnityEngine;
 using UnityEngine.UIElements;
-using Direction = UnityEditor.Experimental.GraphView.Direction;
 
 namespace HLVS.Editor.NodeViews
 {
@@ -13,7 +12,6 @@ namespace HLVS.Editor.NodeViews
 		private GetVariableNode target => nodeTarget as GetVariableNode;
 
 		private TextField _nameField;
-		private bool _isRegisteredKeyDown = false;
 		private Label _titleLabel;
 
 		private ExposedParameter _parameter;
@@ -23,35 +21,17 @@ namespace HLVS.Editor.NodeViews
 			_titleLabel = titleContainer.Q<Label>("title-label");
 
 			_nameField = new TextField("Name");
-			_nameField.RegisterValueChangedCallback(evt =>
-			{
-				target.variableName = evt.newValue;
-				_titleLabel.text = target.name;
-				
-				
-				if (!_isRegisteredKeyDown)
-				{
-					_nameField.RegisterCallback<KeyDownEvent>(OnKeyPressed);
-					_isRegisteredKeyDown = true;
-				}
-			});
-
+			_nameField.value = target.variableName;
+			_nameField.Q<Label>().style.minWidth = 10;
+			_nameField.Q("unity-text-input").style.minWidth = 75;
+			_nameField.RegisterCallback<FocusOutEvent>(e => OnNameChanged());
 			inputContainer.Add(_nameField);
+
+			OnNameChanged();
 		}
 
-		void OnKeyPressed(KeyDownEvent e)
+		private void UpdateVisuals()
 		{
-			if (e.keyCode == KeyCode.Return || e.keyCode == KeyCode.KeypadEnter)
-			{
-				OnNameChanged();
-				_isRegisteredKeyDown = false;
-				_nameField.UnregisterCallback<KeyDownEvent>(OnKeyPressed);
-			}
-		}
-
-		private void OnNameChanged()
-		{
-			_parameter = graph.GetVariable(target.variableName);
 			if (_parameter == null)
 			{
 				Debug.LogError($"No variable called '{target.variableName}' found");
@@ -59,8 +39,24 @@ namespace HLVS.Editor.NodeViews
 			else
 			{
 				// TODO: Add output port
-				mainContainer.Add(new Label($"Value = {_parameter.GetValueType().Name}"));
+				
+				outputContainer.Clear();
+				outputContainer.Add(new Label($"Value = {_parameter.GetValueType().Name}"));
+				RefreshPorts();
 			}
+		}
+
+		private void OnNameChanged()
+		{
+			Undo.RecordObject(owner.graph, "Set GetVariable node");
+			
+			target.variableName = _nameField.value;
+			_titleLabel.text = target.name;
+			_parameter = graph.GetVariable(target.variableName);
+			
+			EditorUtility.SetDirty(owner.graph);
+
+			UpdateVisuals();
 		}
 	}
 }
