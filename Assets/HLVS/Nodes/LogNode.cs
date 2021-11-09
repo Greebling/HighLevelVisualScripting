886163e419272c0333fcdd542b9df9647aec09f5
Greebling <56256331+Greebling@users.noreply.github.com>
2021-11-09 14:09:05 +0100

    Transformed into coroutine like node

--- Assets/HLVS/Nodes/LogNode.cs
+++ Assets/HLVS/Nodes/LogNode.cs
@@ -8,12 +8,35 @@ namespace HLVS.Nodes
 	[Serializable, NodeMenuItem("HLVS/Log")]
 	public class LogNode : HlvsActionNode
 	{
-		[Input("Text")] public string textToLog;
+		[Input("Text")]
+		public string textToLog;
+
+		[Input("Amount")]
+		public int amount;
 
 		public override string name => "Log in Console";
+		
+		[NonSerialized]
+		private int _currAmount = -1;
+
+		public override void Reset()
+		{
+			_currAmount = -1;
+			status = ProcessingStatus.Unfinished;
+		}
 
 		public override void Evaluate()
 		{
+			_currAmount++;
+			if (_currAmount >= amount)
+			{
+				_currAmount = -1;
+				status = ProcessingStatus.Finished;
+				return;
+			}
+
+			status = ProcessingStatus.Unfinished;
+
 			if (!string.IsNullOrEmpty(textToLog))
 				Debug.Log(textToLog);
 		}
