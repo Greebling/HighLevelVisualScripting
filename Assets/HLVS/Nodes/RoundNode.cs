42396854f2b805535cdc75ae426b263da51d13c9
Greebling <56256331+Greebling@users.noreply.github.com>
2021-11-17 13:11:22 +0100

    Add 'Round' converter node from float to int

---
+++ Assets/HLVS/Nodes/RoundNode.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using GraphProcessor;
+
+namespace HLVS.Nodes
+{
+	[Serializable, NodeMenuItem("Math/Round"), ConverterNode(typeof(float), typeof(int))]
+	public class RoundNode : HlvsDataNode, IConversionNode
+	{
+		[Input("In")]
+		public float input;
+		
+		[Output("Out")]
+		public int output;
+
+		public override string name => "Round";
+
+		public string GetConversionInput()
+		{
+			return nameof(input);
+		}
+
+		public string GetConversionOutput()
+		{
+			return nameof(output);
+		}
+	}
+}
