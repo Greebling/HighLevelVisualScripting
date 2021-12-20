959302dd60906e25f248708ad9d16445f3655861
Greebling <56256331+Greebling@users.noreply.github.com>
2021-12-20 12:55:59 +0100

    Added scene management nodes

---
+++ Assets/HLVS/Nodes/ActionNodes/SceneNodes.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using GraphProcessor;
+using UnityEngine.SceneManagement;
+
+namespace HLVS.Nodes.ActionNodes
+{
+	[Serializable, NodeMenuItem("HLVS/Scenes/Load Scene")]
+	public class LoadSceneNode : HlvsActionNode
+	{
+		public override string name => "Load Scene";
+
+		[Input("Scene name")]
+		public string toLoad;
+		
+		[Input("Mode")] public LoadSceneMode loadAdditive = LoadSceneMode.Single;
+
+		public override ProcessingStatus Evaluate()
+		{
+			SceneManager.LoadScene(toLoad, loadAdditive);
+			return ProcessingStatus.Finished;
+		}
+	}
+	
+	[Serializable, NodeMenuItem("HLVS/Scenes/Unload Scene")]
+	public class UnloadSceneNode : HlvsActionNode
+	{
+		public override string name => "Unload Scene";
+
+		[Input("Scene name")]
+		public string toUnload;
+
+		public override ProcessingStatus Evaluate()
+		{
+			SceneManager.UnloadSceneAsync(toUnload);
+			return ProcessingStatus.Finished;
+		}
+	}
+}
