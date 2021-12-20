80b42674e42b2b6e1070ee803dbf68c9f65dc242
Greebling <56256331+Greebling@users.noreply.github.com>
2021-12-20 14:57:13 +0100

    Add GetComponent nodes

---
+++ Assets/HLVS/Nodes/DataNodes/GetComponentsNodes.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using GraphProcessor;
+using UnityEngine;
+
+namespace HLVS.Nodes.DataNodes
+{
+	[Serializable, NodeMenuItem("Components/Get Rigidbody"), ConverterNode(typeof(GameObject), typeof(Rigidbody))]
+	public class GetRigidbodyNode : HlvsDataNode, IConversionNode
+	{
+		public override string name => "Get Rigidbody";
+		
+		[Input("Gameobject")]
+		public GameObject target;
+		
+		[Output("Out")]
+		public Rigidbody output;
+
+		public string GetConversionInput()
+		{
+			return nameof(target);
+		}
+
+		public string GetConversionOutput()
+		{
+			return nameof(output);
+		}
+
+		public override ProcessingStatus Evaluate()
+		{
+			if (!target)
+			{
+				Debug.Assert(false, "No Gameobject given to get a Rigidbody component of");
+				return ProcessingStatus.Finished;
+			}
+
+			output = target.GetComponent<Rigidbody>();
+			
+			Debug.Assert(output, "Given Gameobject does not have a Rigidbody");
+			
+			return ProcessingStatus.Finished;
+		}
+	}
+	
+	[Serializable, NodeMenuItem("Components/Get Animator"), ConverterNode(typeof(GameObject), typeof(Animator))]
+	public class GetLightNode : HlvsDataNode, IConversionNode
+	{
+		public override string name => "Get Animator";
+		
+		[Input("Gameobject")]
+		public GameObject target;
+		
+		[Output("Out")]
+		public Animator output;
+
+		public string GetConversionInput()
+		{
+			return nameof(target);
+		}
+
+		public string GetConversionOutput()
+		{
+			return nameof(output);
+		}
+
+		public override ProcessingStatus Evaluate()
+		{
+			if (!target)
+			{
+				Debug.Assert(false, "No Gameobject given to get an Animator component of");
+				return ProcessingStatus.Finished;
+			}
+
+			output = target.GetComponent<Animator>();
+			
+			Debug.Assert(output, "Given Gameobject does not have an Animator component");
+			
+			return ProcessingStatus.Finished;
+		}
+	}
+}
