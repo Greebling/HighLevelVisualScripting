3c6db767304242b3b4e9f971d996e95cf9e9fea5
Greebling <56256331+Greebling@users.noreply.github.com>
2021-12-21 09:55:57 +0100

    Implement basic debugging nodes

---
+++ Assets/HLVS/Nodes/ActionNodes/DebugNodes.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using GraphProcessor;
+using UnityEditor;
+using UnityEngine;
+
+namespace HLVS.Nodes.ActionNodes
+{
+	[Serializable, NodeMenuItem("Debug/Pause")]
+	public class PauseGameNode : HlvsActionNode
+	{
+		public override string name => "Pause Game";
+
+		public override ProcessingStatus Evaluate()
+		{
+#if UNITY_EDITOR
+			EditorApplication.isPaused = true;
+			Debug.LogWarning("Pausing Game"); // give users a message so they wont wonder why their editor suddenly stopped
+#endif
+			return ProcessingStatus.Finished;
+		}
+	}
+	
+	
+	[Serializable, NodeMenuItem("Debug/Log to Console")]
+	public class ConsoleLogNode : HlvsActionNode
+	{
+		public override string name => "Log to Console";
+
+		[Input("Text")]
+		public string text;
+
+		[Input("Log Type")]
+		public LogType logType = LogType.Default;
+
+		public enum LogType
+		{
+			Default, Warning, Error,
+		}
+
+		public override ProcessingStatus Evaluate()
+		{
+			switch (logType)
+			{
+				case LogType.Default:
+					Debug.Log(text);
+					break;
+				case LogType.Warning:
+					Debug.LogWarning(text);
+					break;
+				case LogType.Error:
+					Debug.LogError(text);
+					break;
+			}
+			
+			return ProcessingStatus.Finished;
+		}
+	}
+}
