c13290f0e72216ab648630827db2bed991a79c0f
Greebling <56256331+Greebling@users.noreply.github.com>
2021-12-22 19:22:18 +0100

    Added FloatToString node

--- Assets/HLVS/Nodes/ParseNodes.cs
+++ Assets/HLVS/Nodes/ParseNodes.cs
@@ -1,5 +1,8 @@
 using System;
+using System.Globalization;
 using GraphProcessor;
+using HLVS.Runtime;
+using UnityEngine;
 
 namespace HLVS.Nodes
 {
@@ -8,7 +11,7 @@ namespace HLVS.Nodes
 	{
 		[Input("In")]
 		public string input;
-		
+
 		[Output("Out")]
 		public int output;
 
@@ -30,13 +33,13 @@ namespace HLVS.Nodes
 			return ProcessingStatus.Finished;
 		}
 	}
-	
+
 	[Serializable, NodeMenuItem("Convert/Read Float from String"), ConverterNode(typeof(string), typeof(float))]
 	public class ParseFloatNode : HlvsDataNode, IConversionNode
 	{
 		[Input("In")]
 		public string input;
-		
+
 		[Output("Out")]
 		public float output;
 
@@ -54,7 +57,69 @@ namespace HLVS.Nodes
 
 		public override ProcessingStatus Evaluate()
 		{
-			output = float.Parse(input);
+			output = float.Parse(input, CultureInfo.InvariantCulture);
+			return ProcessingStatus.Finished;
+		}
+	}
+
+	[Serializable, NodeMenuItem("Convert/Float to String"), ConverterNode(typeof(float), typeof(string))]
+	public class FloatToStringNode : HlvsDataNode, IConversionNode
+	{
+		[Input("In")]
+		public float input;
+
+		[Input("Decimals")]
+		[LargerOrEqual(0)]
+		public int decimalPlace = 2;
+
+		[Output("Out")]
+		public string output;
+
+		public override string name => "To String";
+
+		public string GetConversionInput()
+		{
+			return nameof(input);
+		}
+
+		public string GetConversionOutput()
+		{
+			return nameof(output);
+		}
+
+		public override ProcessingStatus Evaluate()
+		{
+			var mult = Mathf.Pow(10, decimalPlace);
+			float val = Mathf.Round(input * mult) / mult;
+			output = val.ToString(CultureInfo.InvariantCulture);
+			return ProcessingStatus.Finished;
+		}
+	}
+
+	[Serializable, NodeMenuItem("Convert/Int to String"), ConverterNode(typeof(int), typeof(string))]
+	public class IntToStringNode : HlvsDataNode, IConversionNode
+	{
+		[Input("In")]
+		public float input;
+
+		[Output("Out")]
+		public string output;
+
+		public override string name => "To String";
+
+		public string GetConversionInput()
+		{
+			return nameof(input);
+		}
+
+		public string GetConversionOutput()
+		{
+			return nameof(output);
+		}
+
+		public override ProcessingStatus Evaluate()
+		{
+			output = input.ToString(CultureInfo.InvariantCulture);
 			return ProcessingStatus.Finished;
 		}
 	}
