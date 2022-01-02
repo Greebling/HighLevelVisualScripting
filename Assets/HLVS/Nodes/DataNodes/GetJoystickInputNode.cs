21d63e2812593f77209f20843c5fbef14f226d0d
Greebling <56256331+Greebling@users.noreply.github.com>
2022-01-02 22:12:38 +0100

    Remove old input node

--- Assets/HLVS/Nodes/DataNodes/GetJoystickInputNode.cs
+++ Assets/HLVS/Nodes/DataNodes/GetJoystickInputNode.cs
@@ -4,21 +4,6 @@ using UnityEngine;
 
 namespace HLVS.Nodes.DataNodes
 {
-	[Serializable, NodeMenuItem("Input/Axis Input")]
-	public class GetJoystickInputNode : HlvsDataNode
-	{
-		public override          string name => "Get Axis Input";
-		
-		[Input("Axis")]   public string axisName;
-		[Output("Value")] public float  output;
-
-		public override ProcessingStatus Evaluate()
-		{
-			output = Input.GetAxisRaw(axisName);
-			return ProcessingStatus.Finished;
-		}
-	}
-	
 	[Serializable, NodeMenuItem("Input/Get Button Input")]
 	public class GetButtonInputNode : HlvsDataNode
 	{
