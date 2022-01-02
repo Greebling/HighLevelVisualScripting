394116388e3328a025c03e63167d230f60334ae0
Greebling <56256331+Greebling@users.noreply.github.com>
2022-01-02 13:40:51 +0100

    Removed to string parse nodes

--- Assets/HLVS/Nodes/ParseNodes.cs
+++ Assets/HLVS/Nodes/ParseNodes.cs
@@ -6,62 +6,6 @@ using UnityEngine;
 
 namespace HLVS.Nodes
 {
-	[Serializable, NodeMenuItem("Convert/Read Integer from String"), ConverterNode(typeof(string), typeof(int))]
-	public class ParseIntNode : HlvsDataNode, IConversionNode
-	{
-		[Input("In")]
-		public string input;
-
-		[Output("Out")]
-		public int output;
-
-		public override string name => "Read Integer";
-
-		public string GetConversionInput()
-		{
-			return nameof(input);
-		}
-
-		public string GetConversionOutput()
-		{
-			return nameof(output);
-		}
-
-		public override ProcessingStatus Evaluate()
-		{
-			output = int.Parse(input);
-			return ProcessingStatus.Finished;
-		}
-	}
-
-	[Serializable, NodeMenuItem("Convert/Read Float from String"), ConverterNode(typeof(string), typeof(float))]
-	public class ParseFloatNode : HlvsDataNode, IConversionNode
-	{
-		[Input("In")]
-		public string input;
-
-		[Output("Out")]
-		public float output;
-
-		public override string name => "Read Float";
-
-		public string GetConversionInput()
-		{
-			return nameof(input);
-		}
-
-		public string GetConversionOutput()
-		{
-			return nameof(output);
-		}
-
-		public override ProcessingStatus Evaluate()
-		{
-			output = float.Parse(input, CultureInfo.InvariantCulture);
-			return ProcessingStatus.Finished;
-		}
-	}
-
 	[Serializable, NodeMenuItem("Convert/Float to String"), ConverterNode(typeof(float), typeof(string))]
 	public class FloatToStringNode : HlvsDataNode, IConversionNode
 	{
