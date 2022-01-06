e0549b0a06c1ea2abbb55d9fad72b39c71920c60
Greebling <56256331+Greebling@users.noreply.github.com>
2022-01-06 17:40:58 +0100

    Added is same gameobject node

--- Assets/HLVS/Nodes/BranchNode.cs
+++ Assets/HLVS/Nodes/BranchNode.cs
@@ -1,6 +1,7 @@
 using System;
 using GraphProcessor;
 using HLVS.Runtime;
+using UnityEngine;
 
 namespace HLVS.Nodes
 {
@@ -24,6 +25,33 @@ namespace HLVS.Nodes
 		public override string[] GetNextExecutionLinks()
 		{
 			return condition ? new[] { nameof(trueLink) } : new[] { nameof(falseLink) };
+		}
+	}
+	
+	
+	[Serializable, NodeMenuItem("Branch/Is Same Gameobject")]
+	public class Node : HlvsFlowNode
+	{
+		public override string name => "Is Same Gameobject";
+
+		[Input(" ")]
+		public ExecutionLink inputLink;
+
+		[Input("Object")]
+		public GameObject obj1;
+		
+		[Input("Other")]
+		public GameObject obj2;
+
+		[Output("Is Same")]
+		public ExecutionLink trueLink;
+
+		[Output("Is Different")]
+		public ExecutionLink falseLink;
+
+		public override string[] GetNextExecutionLinks()
+		{
+			return obj1 == obj2 ? new[] { nameof(trueLink) } : new[] { nameof(falseLink) };
 		}
 	}
 }
