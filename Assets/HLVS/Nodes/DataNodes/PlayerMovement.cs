6cd1444cded078cc1665d88441c0372c01728a4b
Greebling <56256331+Greebling@users.noreply.github.com>
2022-01-06 17:04:08 +0100

    Update character controller nodes

--- Assets/HLVS/Nodes/DataNodes/PlayerMovement.cs
+++ Assets/HLVS/Nodes/DataNodes/PlayerMovement.cs
@@ -5,45 +5,111 @@ using UnityEngine;
 
 namespace HLVS.Nodes.DataNodes
 {
-	[Serializable, NodeMenuItem("Input/Player Movement")]
-	public class PlayerMovement : HlvsDataNode
+	[Serializable, NodeMenuItem("Input/Top-Down Movement")]
+	public class TopDownMovementNode : HlvsActionNode
 	{
-		public override string name => "Player Movement";
+		public override string name => "Top-Down Movement";
 
-		[Input("Type")]
-		public MovementType inputType;
+		[Input("Object")]
+		public GameObject target;
 
-		[Input("Max Speed")] [LargerThan(0)]
+		[Input("Speed")] [LargerOrEqual(0)]
 		public float maxSpeed = 1;
 
-		[Output("Movement")]
-		public Vector3 direction;
+		[Input("Rotation Speed")] [LargerOrEqual(0)]
+		public float rotSpeed = 180;
 
 		[Output("Speed")]
 		public float speed;
-
-		public enum MovementType
-		{
-			TopDown,
-			SideScroller
-		}
+		
+		[Output("Direction")]
+		public Vector3 movementDir;
 
 		public override ProcessingStatus Evaluate()
 		{
-			switch (inputType)
+			if (!target)
 			{
-				case MovementType.TopDown:
-					direction = new Vector3(Input.GetAxisRaw("Horizontal"), 0, Input.GetAxisRaw("Vertical"));
-					break;
-				case MovementType.SideScroller:
-					direction = new Vector3(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"), 0);
-					break;
-				default:
-					throw new ArgumentOutOfRangeException();
+				Debug.LogWarning("No object given to move");
+				return ProcessingStatus.Finished;
+			}
+			
+			movementDir = new Vector3(Input.GetAxisRaw("Horizontal"), 0, Input.GetAxisRaw("Vertical"));
+			
+			// rotation
+			if(movementDir.sqrMagnitude != 0)
+			{
+				var rot = Quaternion.LookRotation(movementDir);
+				rot = Quaternion.RotateTowards(target.transform.rotation, rot, rotSpeed * Time.deltaTime);
+				target.transform.rotation = rot;
+			}
+			
+			movementDir *= maxSpeed;
+			speed = movementDir.magnitude;
+			
+			// translation
+			var rb = target.GetComponent<Rigidbody>();
+			if (rb)
+			{
+				rb.MovePosition(target.transform.position + Time.deltaTime * movementDir);
+			} else
+			{
+				target.transform.position += Time.deltaTime * movementDir;
 			}
 
-			direction *= maxSpeed;
-			speed = direction.magnitude;
+			return ProcessingStatus.Finished;
+		}
+	}
+
+	[Serializable, NodeMenuItem("Input/Side-Scroller Movement")]
+	public class SideScrollerMovementNode : HlvsActionNode
+	{
+		public override string name => "Side-Scroller Movement";
+
+		[Input("Object")]
+		public GameObject target;
+
+		[Input("Speed")] [LargerOrEqual(0)]
+		public float maxSpeed = 1;
+
+		[Input("Rotation Speed")] [LargerOrEqual(0)]
+		public float rotSpeed = 180;
+
+		[Output("Speed")]
+		public float speed;
+		
+		[Output("Direction")]
+		public Vector3 movementDir;
+
+		public override ProcessingStatus Evaluate()
+		{
+			if (!target)
+			{
+				Debug.LogWarning("No object given to move");
+				return ProcessingStatus.Finished;
+			}
+			
+			movementDir = new Vector3(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"), 0);
+			
+			// rotation
+			if(movementDir.sqrMagnitude != 0)
+			{
+				var rot = Quaternion.LookRotation(movementDir);
+				rot = Quaternion.RotateTowards(target.transform.rotation, rot, rotSpeed * Time.deltaTime);
+				target.transform.rotation = rot;
+			}
+			
+			movementDir *= maxSpeed;
+			speed = movementDir.magnitude;
+			
+			// translation
+			var rb = target.GetComponent<Rigidbody>();
+			if (rb)
+			{
+				rb.MovePosition(target.transform.position + Time.deltaTime * movementDir);
+			} else
+			{
+				target.transform.position += Time.deltaTime * movementDir;
+			}
 
 			return ProcessingStatus.Finished;
 		}
