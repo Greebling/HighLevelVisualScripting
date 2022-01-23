9d000a2b0c2798e9d92917e34fc03cc72ba36eb7
Greebling <56256331+Greebling@users.noreply.github.com>
2022-01-23 15:23:22 +0100

    Fixed dependency exploration of data nodes

--- Assets/HLVS/Nodes/HlvsDataNode.cs
+++ Assets/HLVS/Nodes/HlvsDataNode.cs
@@ -1,4 +1,5 @@
 using System.Linq;
+using GraphProcessor;
 
 namespace HLVS.Nodes
 {
@@ -6,11 +7,39 @@ namespace HLVS.Nodes
 	{
 		public override HlvsNode GetPreviousNode()
 		{
-			// we want to use any data input as all should have the same execution origin
+			// we want to check for non data dependencies first
+			var dependency = GetNonDataDependency();
+			if (dependency != null)
+			{
+				return dependency;
+			}
+			
+			//else  we want to use any data input as all should have the same execution origin
 			var aDataPort = inputPorts.FirstOrDefault(port => port.GetEdges().Count != 0);
 			if (aDataPort != null)
 			{
 				return (HlvsNode) aDataPort.GetEdges().FirstOrDefault()?.outputNode;
+			}
+
+			return null;
+		}
+
+		private HlvsNode GetNonDataDependency()
+		{
+			foreach (BaseNode baseNode in inputPorts.Where(port => port.GetEdges().Count != 0).SelectMany(port => port.GetEdges(), (port, edge) => edge.outputNode))
+			{
+				if (baseNode is not HlvsDataNode dataNode)
+				{
+					return (HlvsNode) baseNode;
+				}
+				else
+				{
+					var dep = dataNode.GetNonDataDependency();
+					if (dep != null)
+					{
+						return dataNode; // we do not return the actual dependency but the previous node that brings us to that dependency
+					}
+				}
 			}
 
 			return null;
