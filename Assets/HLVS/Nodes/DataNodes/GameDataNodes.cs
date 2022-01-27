08b6e85e4fcaeebdfeacacca32445108ab3b794d
Greebling <56256331+Greebling@users.noreply.github.com>
2022-01-27 17:32:34 +0100

    add time data nodes

---
+++ Assets/HLVS/Nodes/DataNodes/GameDataNodes.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using GraphProcessor;
+using UnityEngine;
+
+namespace HLVS.Nodes.DataNodes
+{
+	[Serializable, NodeMenuItem("Data/Game Time")]
+	public class GameTimeNode : HlvsDataNode
+	{
+		public override string name => "Game Time";
+
+		[Output("Current Time")]
+		public float result;
+
+		public override ProcessingStatus Evaluate()
+		{
+			result = Time.time;
+
+			return ProcessingStatus.Finished;
+		}
+	}
+	
+	[Serializable, NodeMenuItem("Data/Delta Time")]
+	public class DeltaTimeNode : HlvsDataNode
+	{
+		public override string name => "Delta Time";
+
+		[Output("Delta Time")]
+		public float result;
+
+		public override ProcessingStatus Evaluate()
+		{
+			result = Time.deltaTime;
+
+			return ProcessingStatus.Finished;
+		}
+	}
+}
